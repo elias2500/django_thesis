522d6a5f13ae311fdffa9ca6576e8a3690080cf2
Ilias Kaleridis <eliaskaler@gmail.com>
2023-06-08 21:01:05 +0300

    Change the delete url to redirect to the confirm delete page.

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('projects/<slug:username>/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('projects/create/', ProjectCreateView.as_view(), name='project_create'),
     path('projects/update/<int:pk>', ProjectUpdateView.as_view(), name='project_update'),
-    path('projects/delete/<int:pk>/', ProjectDeleteView.as_view(), name='project_delete'),
+    path('projects/rooms/delete/<int:pk>/', ProjectDeleteView.as_view(), name='project_delete'),
     path('projects/addriddle/', RiddleAddView.as_view(), name='riddle_add'),
 ]
